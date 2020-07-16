89a407e3dd0b9dccc78bdaf52f9c41f29b47543a
NeiDamazio <Neidamazio@yahoo.com.br>
2020-07-16 10:22:34 -0300

    Commit ate o exercicio 6.14

---
+++ projeto-final/javaScript.js
@@ -0,0 +1,80 @@
+6.1 variáveis e constantes, quero exemplos.
+Variáveis: São locais criados no sistema para armazenar algo. Podem ser alteradas a qualquer momento.
+Constantes: semelhante as variaves, são locais criados no sistema para armazenar algo. Porem, o sistema
+não permite sua alteração. São uteis para valores que devem ser fixos no codigo.
+
+6.2 comandos de decisão, quero exemplos.
+São comandos dentro de codigo que precisam tomar uma decisão a partir de uma condição
+Exemplos (if/else, switch)
+
+6.3 loops, quero exemplos.
+São comandos que irão fazer uma determinada ação varias vezes enquanto a condição imposta for verdade (true)
+Exemplos (for, fon in, for off, while, dowhile) 
+
+6.4 funções, quero exemplos.
+São codigos criados quando precisamos dele diversas vezes ao longo do programa.
+Podemos ter algumas variações
+sem parametros e sem retorno, sem parametros, com retorno, com parametros e sem retorno, com parametros e com retorno
+Exemplo
+function mensagem() {
+   alert("Boa prova!") 
+}
+
+mensagem ()
+
+6.5 escopos, quero exemplos.
+Dentro de um codigo temos os escopos publicos e privados
+Exemplo
+
+uma variavel no escopo publico pode ser acessada de qualquer lugar do codigo.
+Já uma variavel privada so pode ser acessada no local onde ela foi criada, um exemplo disso e uma variavel
+criada dentro de uma função, onde ela so sera acessada e utilizada dentro da mesma, não podendo ser acessada
+fora da função. 
+
+6.6 inputs e outputs, quero exemplos.
+inputs --> comandos para entrada de informações/dados no codigo (prompt())
+outputs --> comandos de saida de informações do codigo (alert(), console.log(), document.write())
+
+6.7 array, quero exemplos.
+Array são listas de dados, onde podemos armazenar diversas linhas de dados em uma mesma variavel.
+Exemplo --> Uma lista de alunos
+var alunos = ["Fulano","Beltrano","Ciclano"];
+
+6.8 objetos, quero exemplos.
+Objetos --> São variaves que possuem diversas informações armazenadas dentro delas 
+Exemplo --> Cliente pode ter diversas informações
+var cliente {nome:"Fulano", Endereço: "Rua A"};
+
+6.9 (camelCase, PascalCase, kibab-case, snake_case).
+camelCase --> Forma de escrita utilizada em diversas linguagens de programação, incluindo o javascript
+Inicia-se com letra minuscula e a cada mudança de palavra inicia-se por letra maiuscula Exemplo: nomeAluno
+PascalCase --> Forma de escrita utilizada em diversas linguagens de programação. Inicia-se com letra
+maiuscula e a cada mudança de palavra inicia-se tambem por letra maiuscula Exemplo: NomeAluno
+kibab-case --> Forma de escrita utilizada em diversas linguagens de programação. 
+Toda escrita em letras minusculas, separando-se cada palavra por tracinho (-) Exemplo: nome-aluno 
+snake_case --> Forma de escrita utilizada em diversas linguagens de programação. 
+Toda escrita em letras minusculas, separando-se cada palavra por underline (_) Exemplo: nome_aluno
+
+6.10 Diferença entre caminho absoluto e caminho relativo.
+Caminho absoluto --> ponto de partida onde esta o arquivo - inicia-se por ./
+Caminho relativo --> Todo o diretorio onde esta o arquivo - Ex.: C:/Serratec/Prova ... 
+
+6.11 Diferença entre os operadores &&, ||, == e !=
+&& = and ou "e"
+|| = Or ou "ou"
+== = igual (=)
+!= = diferente 
+
+6.12 O que é EPER, e para que serve.
+Serve para pegar um problema a ser resolvido e criar uma sepação por partes para melhor entendimento.
+EPER = Entender - Planejar - Executar- Revisar
+
+6.13 Explique o que é Pilha e Fila, quero exemplos.
+Pilha -- primeiro a entrar, ultimo a sair (uma pilha de livros por exemplo)
+Fila -- Primeiro a entrar, primeiro a sair (uma fila de banco por exemplo) 
+
+6.14 Explique com suas palavras qual a diferença entre html, javascript e css
+html -- e o esqueleto programa 
+javascript -- e o cerebro do programa, onde são feitas as funcionalidades do programa 
+css -- A aparencia do programa, onde são feitas a aparencia da tela por exemplo 
+
